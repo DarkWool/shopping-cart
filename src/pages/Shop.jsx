73c4e698bb649b94c2864a84efd59a89987946b9
DarkWool <cooper27@live.com.mx>
2023-05-21 23:57:35 -0600

    Add a loading state to Shop.jsx
    Each time a user changes the selected product category the product list
    will decrease its opacity to indicate that the new products are being
    loaded.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,6 +5,7 @@ import { fetchData } from "../utils";
 
 export function Shop() {
   const { category } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState(null);
   const [categories, setCategories] = useState(null);
 
@@ -19,8 +20,8 @@ export function Shop() {
         );
         setCategories(categoriesData);
       }
-
       setProducts(products);
+      setIsLoading(false);
     })();
   }, [category]);
 
@@ -28,63 +29,70 @@ export function Shop() {
     return <Loader />;
   }
 
+  const changeProductCategory = () => {
+    setIsLoading(true);
+  };
+
+  const greyedStyles = isLoading && "opacity-30";
+
   return (
     products && (
-      <>
-        <div className="container max-w-screen-xl py-20 flex gap-10">
-          <div className="w-52 shrink-0 leading-5">
-            {/* Sidebar */}
-            <h2 className="text-xl font-black pb-5">Categories</h2>
-            {category && (
-              <Link
-                to="/shop"
-                className="text-sm font-medium flex items-center gap-x-2 px-3 py-2 border border-red-500 text-red-500 bg-red-50 mb-2"
+      <div className="container max-w-screen-xl py-20 flex gap-10">
+        <div className="w-52 shrink-0 leading-5">
+          {/* Sidebar */}
+          <h2 className="text-xl font-black pb-5">Categories</h2>
+          {category && (
+            <Link
+              to="/shop"
+              className="text-sm font-medium flex items-center gap-x-2 px-3 py-2 border border-red-500 text-red-500 bg-red-50 mb-4"
+              onClick={changeProductCategory}
+            >
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-                Clear Category
-              </Link>
-            )}
-            <ul>
-              {categories.map((cat, index) => {
-                return (
-                  <li className="mb-2 capitalize" key={index}>
-                    <Link to={`/shop/category/${cat}`}>{cat}</Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+              Clear Category
+            </Link>
+          )}
+          <ul>
+            {categories.map((cat, index) => {
+              return (
+                <li className="mb-2 capitalize" key={index}>
+                  <Link to={`/shop/category/${cat}`} onClick={changeProductCategory}>
+                    {cat}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
-          <div>
-            <h1 className="text-5xl mb-8 tracking-tight">
-              World-class <span className="font-extrabold">composable businesses.</span>
-            </h1>
-            <div className="grid grid-cols-3 gap-5">
-              {products.map((product) => {
-                return (
-                  <div className="flex flex-col p-5 gap-y-1" key={product.id}>
-                    <span className="text-slate-600">{product.category}</span>
-                    <p className="font-semibold leading-5">{product.title}</p>
-                    <span className="font-medium pt-3">${product.price}</span>
-                  </div>
-                );
-              })}
-            </div>
+        <div>
+          <h1 className="text-5xl mb-8 tracking-tight">
+            World-class <span className="font-extrabold">composable businesses.</span>
+          </h1>
+          <div className={`grid grid-cols-3 gap-5 ${greyedStyles}`}>
+            {products.map((product) => {
+              return (
+                <div className="flex flex-col p-5 gap-y-1" key={product.id}>
+                  <span className="text-slate-600">{product.category}</span>
+                  <p className="font-semibold leading-5">{product.title}</p>
+                  <span className="font-medium pt-3">${product.price}</span>
+                </div>
+              );
+            })}
           </div>
         </div>
-      </>
+      </div>
     )
   );
 }
