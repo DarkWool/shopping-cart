6dd041aa0b71527fd3438f7c1dd778be49e06aee
DarkWool <cooper27@live.com.mx>
2023-05-30 23:46:42 -0600

    Add and configure 'jest-dom' package

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
-import { afterEach } from "vitest";
+import { expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
+import matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
