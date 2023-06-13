5c4cf47694e0e6069bdece13a4da2afdeaf175c5
DarkWool <cooper27@live.com.mx>
2023-06-13 02:14:50 -0600

    Improve useScrollDirection hook

--- src/hooks/useScrollDirection.jsx
+++ src/hooks/useScrollDirection.jsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function useScrollDirection() {
   const [scrollDirection, setScrollDirection] = useState(null);
+  const timeoutRef = useRef(null);
 
   useEffect(() => {
     let oldScrollY = window.scrollY;
+
+    const throttle = () => {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = setTimeout(() => getScrollDirection(), 100);
+    };
+
     function getScrollDirection() {
       const currScrollY = window.scrollY;
       const direction = currScrollY > oldScrollY ? "down" : "up";
-      if (direction !== scrollDirection && Math.abs(oldScrollY - currScrollY) > 4) {
+      if (
+        direction !== scrollDirection &&
+        Math.abs(Math.round(oldScrollY - currScrollY)) > 2
+      ) {
         setScrollDirection(direction);
       }
 
       oldScrollY = currScrollY;
     }
 
-    window.addEventListener("scroll", getScrollDirection);
+    window.addEventListener("scroll", throttle);
 
     return () => {
-      window.removeEventListener("scroll", getScrollDirection);
+      window.removeEventListener("scroll", throttle);
+      clearTimeout(timeoutRef.current);
     };
   });
 
