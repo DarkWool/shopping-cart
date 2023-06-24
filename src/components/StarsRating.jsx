8ad73ab57b1f4b7e32d482ce821d9ceaa42be009
DarkWool <cooper27@live.com.mx>
2023-06-23 21:42:28 -0600

    Improve accessibility in StarsRating.jsx

--- src/components/StarsRating.jsx
+++ src/components/StarsRating.jsx
@@ -1,13 +1,25 @@
-export function StarsRating({ itemRating, showRating = false, reviewCount }) {
+import { useId } from "react";
+
+export function StarsRating({ itemRating, showRating = false, reviewCount, className }) {
+  const referenceId = useId();
+
+  if (!itemRating) return null;
+
   return (
-    <div className="flex items-center">
+    <div className={`flex items-center ${className}`}>
       <span
-        className="text-2xl tracking-tighter bg-stars-gradient before:content-['★★★★★'] bg-clip-text text-transparent mr-1"
+        aria-describedby={referenceId}
+        className="users-rating"
         style={{
           "--rating": itemRating,
           "--percent": "calc(var(--rating) / 5 * 100%)",
         }}
       ></span>
+
+      <p
+        id={referenceId}
+        hidden
+      >{`User rating, ${itemRating} out of 5 stars with ${reviewCount} reviews.`}</p>
 
       {showRating && <span className="text-sm font-semibold mr-1">{itemRating}</span>}
 
